43ac833f401442c76509d4002b5f718a448dd8e8
v9 <v9@v9s-MacBook-Pro.local>
2025-04-22 00:56:41 +0000

    maj

--- mobile/app/(tabs)/medications.tsx
+++ mobile/app/(tabs)/medications.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Colors from '@/constants/Colors';
 import { medications } from '@/constants/medical';
-import MedicationCard from '@/components/medical/MedicationCard';
+import MedicationCard from "../../components/medical/MedicationCard";
 import { StatusBar } from 'expo-status-bar';
 import Separator from '@/components/ui/Separator';
 
